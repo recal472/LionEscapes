72577675714798be5b917e66cee7eb58ebd0dc41
recal472 <139476314+recal472@users.noreply.github.com>
2023-07-26 17:35:27 +0100

    new fonts change layout

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Can use the below to apply device screen (not viewport) ratio to an element
+// const screenAR = (el) => el.style.aspectRatio = screen.width / screen.height
+
+// screenAR(document.querySelector("#element"));
+
+
+//idk
+
+// let LionTitleDiv = document.getElementById("front-lion-headline");
+// let LionTitleDivHeight = parseInt(window.getComputedStyle(LionTitleDiv).offsetHeight);
+// LionTitleDiv.style.fontSize =  100 + "px";
+
